c130a5d703925e8ef35e7f09cdb5b47500ad66fd
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2023-10-31 19:30:02 +0000

    Added addCandidate api

---
+++ src/app/api/addCandidate/route.js
@@ -0,0 +1,34 @@
+export async function GET(req, res) {
+    // Make a note we are on
+    // the api. This goes to the console.
+    console.log("in the putInCart api page")
+    // get the values
+    // that were sent across to us.
+    const { searchParams } = new URL(req.url)
+    const candidateID = searchParams.get('candidateID')
+    const pollID = searchParams.get('pollID')
+    
+
+    const ppsn = searchParams.get('ppsn')
+    const name = searchParams.get('name')
+    const address = searchParams.get('address')
+    const email = searchParams.get('email')
+    const phone = searchParams.get('phone')
+    const dateofbirth = searchParams.get('dateofbirth')
+    
+    console.log(pname);
+    // =================================================
+    const { MongoClient } = require('mongodb');
+    const url = 'mongodb+srv://clusterevote202324.zopnvkp.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority';
+    const client = new MongoClient(url);
+    const dbName = 'App'; // database name
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('shopping_cart'); // collection name
+    var myobj = { ppsn: ppsn, candidateID: candidateID};
+    const insertResult = await collection.insertOne(myobj);
+    //==========================================================
+    // at the end of the process we need to send something back.
+    return Response.json({ "data":"" + "inserted" + ""})
+    }
