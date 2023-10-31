e8e48e34f3579537031211ab730e6666e68f2cd3
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2023-10-31 18:25:42 +0000

    Added template from in-class work
    Added completed in-class work from Rich Web Applications; all tasks from course work before reading week.

---
+++ src/app/api/login/route.js
@@ -0,0 +1,16 @@
+export async function GET(req, res) {
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  const pass = searchParams.get('pass')
+  console.log(email);
+  console.log(pass);
+  // database call goes here
+  // at the end of the process we need to send something back.
+  return Response.json({ "data":"valid" })
+  }
+  
---
+++ src/app/register/page.js
@@ -0,0 +1,177 @@
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {ThemeProvider } from '@mui/material/styles';
+
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+
+export default function Page() {
+
+
+
+  /*
+  This function does the actual work
+  calling the fetch to get things from the database.
+  */ 
+  async function runDBCallAsync(url) {
+
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+ 
+    if(data.data== "valid"){
+      console.log("login is valid!")
+
+      
+    } else {
+
+      console.log("not valid  ")
+    }
+  }
+
+
+  /*
+
+  When the button is clicked, this is the event that is fired.
+  The first thing we need to do is prevent the default refresh of the page.
+  */
+	const handleSubmit = (event) => {
+		
+		console.log("handling submit");
+
+
+    event.preventDefault();
+  
+		const data = new FormData(event.currentTarget);
+
+
+    let email = data.get('email')
+		let pass = data.get('pass')
+    let dateofbirth = data.get('dateofbirth')
+    let name = data.get('name')
+
+    console.log("Sent email:" + email)
+    console.log("Sent pass:" + pass)
+    console.log("Sent date of birth:" + pass)
+    console.log("Sent name:" + pass)
+
+
+    runDBCallAsync(`http://localhost:3000/api/register?email=${email}&pass=${pass}&dateofbirth=${dateofbirth}&name=${name}`)
+
+
+
+
+  }; // end handler
+
+
+
+
+  
+  const theme = createTheme({
+    palette: {
+     
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+  
+
+
+
+  
+  return (
+    <ThemeProvider theme={theme}>
+    <Container component="main"  maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="Name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="Date Of Birth"
+            label="Date Of Birth"
+            name="dateofbirth"
+            autoComplete="date-of-birth"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="pass"
+            label="Pass"
+            type="pass"
+            id="pass"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+
+        </Box>
+      </Box>
+
+    </Container>
+
+    </ThemeProvider>
+
+  );
+}
