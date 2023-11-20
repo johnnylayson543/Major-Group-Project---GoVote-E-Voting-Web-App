0968ba71f87b9e35cb77b5968f437a2aac4f6a8d
SabreClaw86 <114063362+SabreClaw86@users.noreply.github.com>
2023-11-20 14:56:54 +0000

    Updated Database Connection Methods
    mongoDBCloud is modified based on KrispyKreme trial and error findings.

--- src/app/api/database/mongoDBCloud.js
+++ src/app/api/database/mongoDBCloud.js
@@ -1,22 +1,30 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const credentials = 'src\app\api\database\X509-cert-210263698362705703.pem'
 
-const client = new MongoClient('mongodb+srv://cluster0.sy1ddlq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
+const dbName = "KrisptKreme";
+
+
+const url = 'mongodb+srv://cluster0.sy1ddlq.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority';
+const security1 = {
   tlsCertificateKeyFile: credentials,
   serverApi: ServerApiVersion.v1
-});
+};
 
-export async function getClient() {
+const client = new MongoClient(url , security1);
+
+async function getClient() {
   try {
     await client.connect();
-    return client;
+    console.log("Client found. ");
     // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    return client;
+  } catch (error) {
+    console.error("Failed to connect to MongoDB",error);
+    throw error;
   }
 }
 
-//run().catch(console.dir);
+module.exports = { getClient, client };
+// run().catch(console.dir);
+
