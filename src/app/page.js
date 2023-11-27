61fd2a5b15e9af17e312e3438793066f08029bf8
johnnylayson543 <B00148227@mytudublin.ie>
2023-11-27 14:44:31 +0000

    Added grid and boxes on the index page
    - Added grid and boxes on the index page (home page).

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,16 @@ import NavBar from './/header/navBar'; // Adjust the path based on your project 
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-
-/*
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import ButtonBase from '@material-ui/core/ButtonBase';
-*/
 
 global.mongoURL = "mongodb+srv://evote.kyxphj1.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+
+const Item = ({ children }) => (
+    <Box sx={{ border: '4px solid #ddd', padding: 4 }}>
+      {children}
+    </Box>
+  );
 
 
 export default function page() {
@@ -22,12 +21,26 @@ export default function page() {
         <Box component="main" sx={{ p: 3 }}>
             <NavBar/>
             <Toolbar/>
-            <Typography>
+            <Grid container spacing={6}>
+                <Grid item xs={8}>
+                <Item>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
                 fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
                 aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-                cum quibusdam
-            </Typography>
+                cum quibusdam.
+                <br></br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris et nulla ac eros aliquet suscipit ut quis libero. 
+                Sed quam erat, dictum in consequat id, dapibus eu libero.
+                </Item>
+                </Grid>
+                 <Grid item xs={4}>
+                 <Item>Another Login Page Could Go Here!</Item>
+                 </Grid>
+                 <Grid item xs={8}>
+                 <Item>Some Photos go here</Item>
+                 </Grid>
+            </Grid>
         </Box>
     );
 
