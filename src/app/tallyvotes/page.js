ff3066eaa74732614b160439ca8df7a891e25b3d
johnnylayson543 <B00148227@mytudublin.ie>
2024-02-19 15:18:26 +0000

    Cleaned the tallyvotes page
    - Continued to clean-up tallyvotes page even if it doesn't currently work at the moment.

--- src/app/tallyvotes/page.js
+++ src/app/tallyvotes/page.js
@@ -1,20 +1,9 @@
 'use client';
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+
 import Box from '@mui/material/Box';
 import Chart from 'chart.js/auto'; // Add this line
 import NavBar from '../header/navBar';
-
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import {ThemeProvider } from '@mui/material/styles';
 
 import Script from 'next/script'
 import { useState, useEffect } from 'react'
