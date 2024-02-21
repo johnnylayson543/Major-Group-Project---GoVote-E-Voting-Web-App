e067ec3e025b7e4c2e229b314a42a373ec72a458
YKMesso <B00142467@mytudublin.ie>
2024-02-21 17:38:44 +0000

    h2 tag fix
    removed error on abouts and contact page regarding h2 tag

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -19,14 +19,14 @@ export default function Page() {
                 Welcome to GoVote
             </Typography>
             <Typography>
-                <h2>Our Mission: Making Voting Inclusive and Accessible to ALL</h2>
+                 <span style={{ fontSize: '25px', fontWeight: '700', display: 'block', padding: '15px' }}>Our Mission: Making Voting Inclusive and Accessible to ALL</span>
 
                 At GoVote, we believe in the power of democracy and the importance of every voice being heard.
                 Inspired by the success of Estonia's e-voting system and fueled by the desire to empower every eligible voter,
                 we set out on a journey to create a platform that makes voting more inclusive and accessible, regardless of your location or situation.
 
                 <br/>
-                <h2>The Genesis of GoVote</h2>
+                <span style={{ fontSize: '25px', fontWeight: '700', display: 'block', padding: '15px' }}>The Genesis of GoVote</span>
                 The idea was born when our founders, John Layson, Adam O'Shea,
                 and Emmanuel Ojomo, recognized the transformative impact of e-voting in Estonia. They were moved by stories
                 of individuals unable to cast their votes due to being abroad during crucial elections that directly affected them.
@@ -34,14 +34,14 @@ export default function Page() {
                 no matter where life takes them.
 
                 <br/>
-                <h2>Our Commitment to Inclusivity</h2>
+                <span style={{ fontSize: '25px', fontWeight: '700', display: 'block', padding: '15px' }}>Our Commitment to Inclusivity</span>
                 At GoVote, inclusivity is at the heart of everything we do. Our mission is to break down barriers
                 and ensure that every eligible voter has the opportunity to exercise their democratic right. Whether you're living abroad,
                 facing mobility challenges, or simply prefer the convenience of online voting, our platform is designed
                 to make the electoral process seamless and accessible.
 
                 <br/>
-                <h2>Join Us in Shaping the Future of Democracy</h2>
+                <span style={{ fontSize: '25px', fontWeight: '700', display: 'block', padding: '15px' }}>Join Us in Shaping the Future of Democracy</span>
                 We invite <b>you</b> to be part of this democratic revolution. Your voice matters, and with GoVote,
                 you can make it heard. Together, let's pave the way for a more inclusive and accessible electoral system.
 
