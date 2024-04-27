ff9e2a98c9276b3636154314a56360530b044c8a
SabreClaw86 <b00147637@mytudublin.ie>
2024-04-27 08:20:58 +0100

    Update

--- src/app/components/theme.js
+++ src/app/components/theme.js
@@ -10,7 +10,7 @@ import {blue, green, purple} from '@mui/material/colors';
 const theme = createTheme({
   palette: {
     background: {
-        default: "#6c757d", // Set the Background Color
+        default:  'lch(48.68% 6.04 241.68)' ,// Set the Background Color  { initial lch(45.24% 32.9 241.68) "#18749b",  modified lch(48.68% 6.04 248.87) "#6c757d" } lch more precise.
     },
     primary: {
       main: '#00008B', // Set the primary color (e.g. Buttons)
@@ -22,7 +22,45 @@ const theme = createTheme({
   typography: {
     // You can customize typography styles here
   },
+  components: {
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          // Apply styles to all buttons
+          fontSize: '1rem',
+          padding: '0.8rem 1.6rem',
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          // Apply styles to all buttons
+          fontSize: '1rem',
+          padding: '0.8rem 1.6rem',
+        },
+        contained: {
+          // Apply styles to contained buttons
+          backgroundColor: 'lch(48.68% 6.04 241.68)',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: 'lch(48.68% 6.04 248.87)',
+          },
+        },
+        '#myCustomButton': {
+          // Apply styles to a button with the ID "myCustomButton"
+          fontWeight: 'bold',
+          color: '#f00',
+        },
+        '.my-custom-class': {
+          // Apply styles to elements with the class "my-custom-class"
+          textTransform: 'uppercase',
+        },
+      },
+    },
+  },
   // Other theme customization options
 });
+
 
 export default theme;
